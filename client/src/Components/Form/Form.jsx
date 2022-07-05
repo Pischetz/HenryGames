4ca36e02e6b091585c0636fad99388e4904d55bc
Francisco <franciscopischetz@gmail.com>
2022-07-05 15:14:34 -0300

    Features Front funcionales 95%

--- client/src/Components/Form/Form.jsx
+++ client/src/Components/Form/Form.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react'
 import { connect } from 'react-redux'
-import { getGenres } from '../../Actions/Actions'
+import { getGenres, postGames } from '../../Actions/Actions'
 import './Form.css'
 
 function Form(props){
@@ -8,12 +8,11 @@ function Form(props){
     let [error, setError] = useState({})
     let [formName, setFormName] = useState('')
     let [formDescription, setFormDescription] = useState('')
-    let [formRelease, setFormRelease] = useState('')
-    let [formRating, setFormRatig] = useState(0)
+    let [formRating, setFormRating] = useState(0)
     let [formPlatforms, setFormPlatforms] = useState([])
     let [formGenres, setFormGenres] = useState([])
     const platforms = ['Playstation 4', 'Playstation 5', 'Xbox', 'Windows', 'Linux', 'Mac', 'Android', 'Ios' ]
-    const {genres, getGenres} = props
+    const {genres, getGenres, postGames} = props
 
     useEffect(() => {
         if(!genres.length){
@@ -22,9 +21,34 @@ function Form(props){
     },[genres, getGenres])
 
 
-    function handleSubmit(){
-
+    function handleSubmit(e){
+        e.preventDefault()
+        postGames({
+            name: formName,
+            description: formDescription,
+            rating: formRating,
+            platforms: formPlatforms,
+            genres: formGenres,
+            release: date
+        })
     }
+    function handleRating(e){
+        setFormRating(e.target.value)
+    }
+    function handleName(e){
+        setFormName(e.target.value)
+    }
+    function handleDescription(e){
+        setFormDescription(e.target.value)
+    }
+    function handlePlatforms(e){
+        if(formPlatforms.includes(e.target.value)){
+            setFormPlatforms(formPlatforms.filter(p => p !== e.target.value))
+        }else{
+            setFormPlatforms([...formPlatforms, e.target.value])
+        }
+    }
+
     function handleGenres(e){
         if(formGenres.includes(e.target.value)){
             setFormGenres(formGenres.filter(g => g !== e.target.value))
@@ -32,24 +56,22 @@ function Form(props){
             setFormGenres([...formGenres, e.target.value])
         }
     }
-
     function handleDate(e){
         setDate(e.target.value)
-        console.log(date)
     }
 
     return <div className="form">
         <h1>Add a New Game</h1>
-        <form>
+        <form onSubmit={handleSubmit}>
         <div className='formContainer'>
         <div className='leftContainer'>
             <label htmlFor='name' className='labels'>
             Name
-            <input type={'text'} id='name'/>
+            <input type={'text'} id='name' value={formName} onChange={handleName}/>
             </label>
             <label htmlFor='description' className='labels'>
             Description
-            <textarea id='description'/>
+            <textarea id='description' value={formDescription} onChange={handleDescription}/>
             </label>
             <label htmlFor='dateInput' className='labels'>
             Released
@@ -57,14 +79,14 @@ function Form(props){
             </label>
             <label htmlFor='ratingInput' className='labels'>
             Rating
-            <input type={'number'} id='ratingInput'/>
+            <input type={'number'} id='ratingInput' value={formRating} onChange={handleRating}/>
             </label>
         </div>
         <div className='rightContainer'>
             <label htmlFor='platforms' className='labels'>
             Platforms
             <div className='genresDiv'>
-            {platforms.map(p => <input type={'checkbox'} label={p} className='genresChecks' key={p} value={p} name={'plat'}/>)}
+            {platforms.map(p => <input type={'checkbox'} label={p} className='genresChecks' key={p} value={p} name={'plat'} onChange={handlePlatforms}/>)}
             </div>
             </label>
             <label htmlFor='genres' className='labels'>
@@ -75,6 +97,7 @@ function Form(props){
             </label>
         </div>
         </div>
+        <input type={'submit'}/>
         </form>
     </div>
 }
@@ -88,6 +111,7 @@ function mapStateToProps(state){
 function mapDispatchToProps(dispatch){
     return {
         getGenres: store => dispatch(getGenres(store)),
+        postGames: store => dispatch(postGames(store))
     }
 }
 
